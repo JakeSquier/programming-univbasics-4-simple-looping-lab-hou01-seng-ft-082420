ae252889a595f3090148eef7f82b434a55aa1a04
Jacob Squier <jacob.squier@gmail.com>
2020-06-21 21:45:57 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,26 +1,2 @@
-def loop_message_five_times(message)
-  counter = 0
-  while counter < 5 do
-    puts message
-    counter++
-  end
-end
-
 def 
-  loop_message_n_time(message, limit) do
-  counter = 0
-  while counter < limit
-    puts message
-    counter++
-  end
-  }
-end
-
-def output_array(array) do
-  counter = 0 
-  while counter < array.length
-    puts array[counter]
-    counter++
-  end
-end
   
